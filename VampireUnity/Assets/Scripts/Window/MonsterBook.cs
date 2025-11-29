52a754e620a15f661a95cc1575e1c24dbedf8fae
wangweikang3 <wangweikang3@tal.com>
2025-11-29 17:09:29 +0800

    包管理器

--- VampireUnity/Assets/Scripts/Window/MonsterBook.cs
+++ VampireUnity/Assets/Scripts/Window/MonsterBook.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using Spine.Unity;
 using UnityEngine;
-using UnityEngine.U2D.Animation;
 using UnityEngine.UI;
 
 public class MonsterBook : MonoBehaviour
